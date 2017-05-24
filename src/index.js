a7316758d303aee05eeda93a4d2b1c1e3c3ffc5a
Joakim J. Rapp <joakimrapp@hotmail.com>
2017-05-24 16:49:10 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -10,34 +10,31 @@ const modules = Object.defineProperties( {}, {
 } );
 class ProtoParser {
 	static isService( reflectionObject ) { return reflectionObject instanceof modules.protobufjs.Service; }
-	static getMethodsRecursive( reflectionObject ) { return ProtoParser.isService( reflectionObject ) ?
-		reflectionObject.methodsArray.map( method => method.resolve() ) :
-		Array.prototype.concat( ...reflectionObject.nestedArray.map( ProtoParser.getMethodsRecursive ) ); }
-	constructor() { return Object.assign( this, { context: { absolutepaths: [], jsons: [] } } ); }
-	get promise() { return ( this.context.promise = ( this.context.promise || Promise.resolve() ) ); }
-	set promise( value ) { this.context.promise = value; }
-	get isResolved() { return this.context.absolutepaths.length + this.context.jsons.length === 0; }
-	get resolved() { return ( this.promise = this.promise.then( () => this.isResolved ?
-		this.context.root :
-		Promise.all( this.context.absolutepaths.splice( 0 ).map( absolutepath => modules.files( absolutepath, '*.proto' ) ) )
-			.then( result => [ ...new Set( Array.prototype.concat( ...result ) ) ] )
-			.then( filepaths => ( this.context.root || modules.protobufjs ).load( filepaths ) )
-			.then( root => this.context.jsons.splice( 0 ).reduce( ( root, json ) => root.addJSON( json ), ( this.context.root = root ) ) ) ) );
+	static getMethodsRecursive( reflectionObject ) {
+		return ProtoParser.isService( reflectionObject ) ? reflectionObject.methodsArray.map( method => method.resolve() ) :
+		Array.isArray( reflectionObject.nestedArray ) ? Array.prototype.concat( ...reflectionObject.nestedArray.map( ProtoParser.getMethodsRecursive ) ) :
+	 	[];
 	}
+	constructor() { return Object.assign( this, { promise: Promise.resolve(), root: new modules.protobufjs.Root() } ); }
 	get add() { return {
 		path: ( scanpath = '.' ) => {
-			const absolutepath = path.isAbsolute( scanpath ) ? scanpath : path.resolve( path.dirname( modules.stacktrace.get()[ 1 ].getFileName() ), scanpath );
-			this.promise = this.promise.then( () => this.context.absolutepaths.push( absolutepath ) );
+			const absolutepath = path.normalize( path.isAbsolute( scanpath ) ? scanpath : path.resolve( path.dirname( modules.stacktrace.get()[ 1 ].getFileName() ), scanpath ) );
+			const basepath = path.dirname( absolutepath );
+			this.promise = this.promise
+				.then( () => modules.files( absolutepath, '*.proto' ) )
+				.then( absolutepaths => absolutepaths.map( absolutepath => absolutepath.slice( basepath.length + 1 ) ) )
+				.then( relativepaths => Object.assign( this.root, { resolvePath: ( origin, target ) =>
+					path.resolve( target.indexOf( 'google' ) === 0 ? path.resolve( path.dirname( require.resolve( 'protobufjs' ) ) ) : basepath, target ) } ).load( relativepaths ) );
 			return this;
 		},
 		json: ( json ) =>  {
-			this.promise = this.promise.then( () => this.context.jsons.push( json ) );
+			this.promise = this.promise.then( () => this.root.addJSON( json ) );
 			return this;
 		}
 	}; }
-	methods( ...args ) { this.promise = this.resolved.then( root => ProtoParser.getMethodsRecursive( root ) ).then( ...args ); return this; }
-	json( ...args ) { this.promise = this.resolved.then( root => root.toJSON().nested ).then( ...args ); return this; }
-	then( ...args ) { this.promise = this.resolved.then( ...args ); return this; }
+	methods( ...args ) { this.promise = this.promise.then( () => ProtoParser.getMethodsRecursive( this.root ) ).then( ...args ); return this; }
+	json( ...args ) { this.promise = this.promise.then( () => this.root.toJSON().nested ).then( ...args ); return this; }
+	then( ...args ) { this.promise = this.promise.then( () => this.root ).then( ...args ); return this; }
 	catch( ...args ) { this.promise = this.promise.catch( ...args ); return this; }
 }
 module.exports = () => new ProtoParser();
