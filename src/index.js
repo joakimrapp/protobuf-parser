d96f57e1f29e5304ed59df5e93cd2591e041d614
Joakim J. Rapp <joakim.rapp@viaplay.com>
2017-05-24 12:16:13 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const path = require( 'path' );
 const log = require( '@jrapp/log-emitter' ).log( 'protobuf-parser' );
-require( '@jrapp/log-emitter' ).on( '*', console.log );
 const modules = Object.defineProperties( {}, {
 	files: { configurable: true, get: function() { return Object.defineProperty( this, 'files', { value: log.timer()
 		.set( require( '@jrapp/files-finder' ) ).metric( 'loaded module', '@jrapp/files-finder' ).get() } ).files; } },
@@ -10,12 +9,13 @@ const modules = Object.defineProperties( {}, {
 		.set( require( 'stack-trace' ) ).metric( 'loaded module', 'stack-trace' ).get() } ).stacktrace; } }
 } );
 const getAbsolutePath = scanpath => path.isAbsolute( scanpath ) ? scanpath : path.resolve( path.dirname( modules.stacktrace.get()[ 2 ].getFileName() ), scanpath );
-const resolve = ( { context } ) => context.promise
-	.then( () => Promise.all( context.absolutepaths.splice( 0 ).map( absolutepath => modules.files( absolutepath, '*.proto' ) ) ) )
-	.then( result =>  ( context.filepaths = [ ...new Set( context.filepaths.concat( Array.prototype.concat( ...result ) ) ) ] ) )
-	.then( filepaths => ( context.root || modules.protobufjs ).load( filepaths.splice( 0 ) ) )
-	.then( root => ( context.root = root ) )
-	.then( root => context.jsons.splice( 0 ).reduce( ( root, json ) => root.addJSON( json ), root ) );
+const isResolved = ( { absolutepaths, jsons } ) => absolutepaths.length + jsons.length === 0;
+const resolve = ( { context } ) => context.promise.then( () => isResolved( context ) ? context.root :
+	Promise.all( context.absolutepaths.splice( 0 ).map( absolutepath => modules.files( absolutepath, '*.proto' ) ) )
+		.then( result =>  ( context.filepaths = [ ...new Set( context.filepaths.concat( Array.prototype.concat( ...result ) ) ) ] ) )
+		.then( filepaths => ( context.root || modules.protobufjs ).load( filepaths.splice( 0 ) ) )
+		.then( root => ( context.root = root ) )
+		.then( root => context.jsons.splice( 0 ).reduce( ( root, json ) => root.addJSON( json ), root ) ) );
 class ProtoParser {
 	constructor() { return Object.assign( this, { context: {
 		promise: undefined,
@@ -26,7 +26,7 @@ class ProtoParser {
 	} } ); }
 	get promise() { return ( this.context.promise = ( this.context.promise || Promise.resolve() ) ); }
 	set promise( value ) { this.context.promise = value; }
-	json( ...args ) { this.promise = this.promise.then( root => root.toJSON().nested ).then( ...args ); return this; }
+	json( ...args ) { this.promise = resolve( this ).then( root => root.toJSON().nested ).then( ...args ); return this; }
 	get add() { return {
 		path: ( scanpath = '.' ) => {
 			const absolutepath = getAbsolutePath( scanpath );
--- test/src/index.js.test.js
+++ test/src/index.js.test.js
@@ -1,10 +1,19 @@
 require( '@jrapp/node-project-setup' ).testing.file( './test' )( ( index ) => {} )
-	.it( 'should', ( assert, index ) => index()
-		.add.path( '../assets/folder1' )
+	.it( 'should run a sanity check', ( assert, index ) => index()
+		.add.path( '../assets/ok/folder1' )
 		.add.json( { "test": { "nested": {
 			"RequestMessage2": { "fields": { "input": { "type": "string", "id": 1 } } },
-      "ResponseMessage2": { "fields": { "output": { "type": "string", "id": 1 } } },
-      "Service2": { "methods": { "Method2": { "requestType": "RequestMessage2", "responseType": "ResponseMessage2" } } }
+			"ResponseMessage2": { "fields": { "output": { "type": "string", "id": 1 } } },
+			"Service2": { "methods": { "Method2": { "requestType": "RequestMessage2", "responseType": "ResponseMessage2" } } }
 		} } } )
-	 	.then( root => assert.ok( !!root.lookup( 'test.Service2' ) ) ) )
+		.then( root => assert.ok( !!root.lookup( 'test.Service2' ) ) ) )
+	.it( 'should scan for all proto-files', ( assert, index ) => index()
+		.add.path( require( 'path' ).resolve( __dirname, '../assets/ok' ) )
+		.then( root => assert.ok( !!root.lookup( 'test.Service2' ) && !!root.lookup( 'test.Service1' ) ) ) )
+	.it( 'should catch errors', ( assert, index ) => new Promise( resolve => index()
+		.add.path( '../assets/fail' )
+	 	.then( () => assert.fail() )
+	 	.catch( () => resolve() ) ) )
+	.it( 'should output json', ( assert, index ) => index().add.path( '../assets/ok' )
+		.json( ( json ) => assert.ok( json ) ) )
 	.done();
